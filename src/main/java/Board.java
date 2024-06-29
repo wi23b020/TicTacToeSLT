eafb6f5dc6e91f9cfc49a6e2a2e9e9e8245a5002
wi23b020 <git@github.com>
2024-06-29 14:19:05 +0200

    second commit

---
+++ src/main/java/Board.java
@@ -0,0 +1,52 @@
+public class Board {
+    public char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+        }
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cells[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println("Current board state:");
+        System.out.println("-------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j] == ' ' ? ' ' : cells[i][j]);
+                if (j < 2) System.out.print("|");
+                if (j == 2) System.out.println("| ");
+
+            }
+        }
+        System.out.println("-------");
+    }
+}
---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
