756861c714f17be3cc352bff9229a9abfdae6d03
wi23b020 <git@github.com>
2024-06-30 11:45:05 +0200

    final changes

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -100,4 +100,16 @@ public class TicTacToe {
             }
         }
     }
+
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Willkommen bei Tic-Tac-Toe!");
+        game.start(scanner);
+
+    }
 }
+
+
